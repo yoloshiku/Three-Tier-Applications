7fc2e1cbc478de71e3240155f5809c21acfb2812
yoloshiku <yoloshiku@gmail.com>
2025-10-15 01:41:17 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,17 @@
-# ---------------------------------------
-# Data Sources (look up OCI resources)
-# ---------------------------------------
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+
+# 🔹 Get list of availability domains
 data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_ocid
 }
 
+# 🔹 Get Canonical Ubuntu 22.04 Minimal ARM image
 data "oci_core_images" "ubuntu_2204_minimal_arm" {
   compartment_id           = var.compartment_ocid
   operating_system         = "Canonical Ubuntu"
@@ -14,24 +21,26 @@ data "oci_core_images" "ubuntu_2204_minimal_arm" {
   sort_order               = "DESC"
 }
 
-# ---------------------------------------
-# Create Compute Instance
-# ---------------------------------------
+# 🔹 Create an OCI instance using the Ubuntu image
 resource "oci_core_instance" "vm_instance" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_ocid
-  display_name        = var.instance_name
+  display_name        = "ubuntu-22-04-minimal-instance"
   shape               = var.instance_shape
+
+  shape_config {
+    ocpus         = 1
+    memory_in_gbs = 6
+  }
 
   create_vnic_details {
     subnet_id        = var.subnet_ocid
     assign_public_ip = true
-    hostname_label   = var.instance_name
   }
 
   source_details {
     source_type = "image"
-    source_id   = data.oci_core_images.oracle_linux.id
+    source_id   = data.oci_core_images.ubuntu_2204_minimal_arm.images[0].id
   }
 
   metadata = {
