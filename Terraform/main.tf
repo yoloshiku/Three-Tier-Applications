f5c0e370a18d0ae46a99f042eac32ad8d4bac599
yoloshiku <yoloshiku@gmail.com>
2025-10-16 02:34:05 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,23 +1,15 @@
-# 🔹 Get list of availability domains
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.tenancy_ocid
-}
-
-# 🔹 Get Canonical Ubuntu 22.04 Minimal ARM image
 data "oci_core_images" "ubuntu_2204_minimal_arm" {
   compartment_id           = var.compartment_ocid
   operating_system         = "Canonical Ubuntu"
-  operating_system_version = "22.04 Minimal"
-  shape                    = var.instance_shape
+  operating_system_version = "22.04"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
 }
 
-# 🔹 Create an OCI instance using the Ubuntu image
 resource "oci_core_instance" "vm_instance" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_ocid
-  display_name        = "ubuntu-22-04-minimal-instance"
+  display_name        = "ubuntu-22-04-instance"
   shape               = var.instance_shape
 
   shape_config {
@@ -36,6 +28,6 @@ resource "oci_core_instance" "vm_instance" {
   }
 
   metadata = {
-    ssh_authorized_keys = file(var.ssh_public_key_path)
+    ssh_authorized_keys = file(pathexpand(var.ssh_public_key_path))
   }
 }
