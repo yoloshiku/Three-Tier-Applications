ca60bf06e4534e3334bbb646d1d3a9eed294ad8d
yoloshiku <yoloshiku@gmail.com>
2025-10-16 02:28:44 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,11 +1,3 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
-}
-
 # 🔹 Get list of availability domains
 data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_ocid
