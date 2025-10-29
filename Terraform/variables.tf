f02b64750ee837639ec76f7e7bdd06dfaa587d99
yoloshiku <yoloshiku@gmail.com>
2025-10-29 00:21:26 -0400

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,45 +1,13 @@
-variable "tenancy_ocid" {
-  description = "OCI tenancy OCID"
-  type        = string
-}
-
-variable "user_ocid" {
-  description = "OCI user OCID"
-  type        = string
-}
-
-variable "fingerprint" {
-  description = "OCI API key fingerprint"
-  type        = string
-}
-
-variable "private_key_path" {
-  description = "Path to your OCI API private key"
-  type        = string
-}
-
-variable "region" {
-  description = "OCI region"
-  type        = string
-}
-
-variable "compartment_ocid" {
-  description = "Compartment OCID where instance will be created"
-  type        = string
-}
-
-variable "subnet_ocid" {
-  description = "Existing subnet OCID"
-  type        = string
-}
-
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+variable "compartment_ocid" {}
+variable "availability_domain" {}
 variable "instance_shape" {
-  description = "Instance shape"
-  type        = string
-  default     = "VM.Standard.A1.Flex"
+  default = "VM.Standard.A1.Flex" # same as KubeMaster
 }
-
 variable "ssh_public_key_path" {
-  description = "Path to your SSH public key file"
-  type        = string
+  default = "~/.ssh/id_rsa.pub"
 }
